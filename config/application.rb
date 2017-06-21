1c0e7d9193b9305b1b2c1353c8fa598c41e5c000
Chris McCammon <cmccammon@gmail.com>
2017-06-21 16:20:57 -0700

    heroku asset pipline fix

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -11,5 +13,6 @@ module WrenchRoverApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.serve_static_assets = true
   end
 end
