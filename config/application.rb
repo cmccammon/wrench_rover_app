e5d4589b93cf0e3d24a2d2a7ddac67beae9a3585
Chris McCammon <cmccammon@gmail.com>
2017-06-21 17:57:14 -0700

    testing jquery

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -13,6 +14,7 @@ module WrenchRoverApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.serve_static_assets = true
+
   end
+
 end
