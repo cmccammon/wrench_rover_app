7947dd2ce38ebb56e3ca8d4716347a9999163352
Chris McCammon <cmccammon@gmail.com>
2017-09-27 09:07:36 -0700

    added completed tab to sc dash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-
   def after_sign_in_path_for(resource)
      # check for the class of the object to determine what type it is
      if resource.class == User && resource.sign_in_count == 1
